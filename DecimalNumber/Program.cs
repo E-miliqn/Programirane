c3bb990adaccde4818fac963b4923617afd30685
E-Eremiev <misterskriberr@gmail.com>
2025-04-28 21:22:23 +0300

    zad

---
+++ DecimalNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DecimalNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int Nsystem = int.Parse(input[0]);
+            int decimalNumber = int.Parse(input[1]);
+
+            if (Nsystem < 2 || Nsystem > 10)
+            {
+                Console.WriteLine("Невалидна основа. Въведете основа между 2 и 10.");
+                return;
+            }
+            
+        }
+    }
+}
---
+++ N system/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace N_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] input = Console.ReadLine().Split(' ');
+            int Nsystem = int.Parse(input[0]);
+            int decimalNumber = int.Parse(input[1]);
+
+            if (Nsystem < 2 || Nsystem > 10)
+            {
+                Console.WriteLine("Невалидна основа. Въведете основа между 2 и 10.");
+                return;
+            }
+            string result = "";
+            while (decimalNumber > 0)
+            {
+                int ost = decimalNumber % Nsystem;
+                result = ost.ToString() + result;
+                decimalNumber /= Nsystem;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
+    
+
